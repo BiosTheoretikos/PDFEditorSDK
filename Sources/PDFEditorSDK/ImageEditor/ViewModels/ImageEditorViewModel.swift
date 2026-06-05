e11f1c11161e5dad7272f2384060b7d3cc5008dc
Brian <BiosTheoretikos@users.noreply.github.com>
2026-06-04 17:38:42 -0700

    Move image undo model out of view model

--- Sources/PDFEditorSDK/ImageEditor/ViewModels/ImageEditorViewModel.swift
+++ Sources/PDFEditorSDK/ImageEditor/ViewModels/ImageEditorViewModel.swift
@@ -445,16 +445,3 @@ class ImageEditorViewModel {
 
 // MARK: - PencilGestureHandler Conformance
 extension ImageEditorViewModel: PencilGestureHandler {}
-
-// MARK: - ImageUndoAction
-
-enum ImageUndoAction {
-    case stroke(add: InkStroke?, remove: InkStroke?)
-    case eraseSession(old: [InkStroke], new: [InkStroke])
-    case textBox(add: OverlayTextBoxState?, remove: OverlayTextBoxState?)
-    case textBoxUpdate(before: OverlayTextBoxState, after: OverlayTextBoxState)
-    case imageBox(add: OverlayImageState?, remove: OverlayImageState?)
-    case imageBoxUpdate(before: OverlayImageState, after: OverlayImageState)
-    case imageShape(add: OverlayShapeState?, remove: OverlayShapeState?)
-    case imageShapeUpdate(before: OverlayShapeState, after: OverlayShapeState)
-}
